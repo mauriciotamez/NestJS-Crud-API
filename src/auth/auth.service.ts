24be88c690588b567169bd83824a54ce451e29ad
Mauricio Tamez <vitxlssreturn@gmail.com>
2022-03-17 18:36:13 -0600

    Created the logic for the signin and signup functions, and hashed the passwords with argon.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,68 @@
-import { Injectable } from '@nestjs/common'
+import {
+	ForbiddenException,
+	Injectable
+} from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { AuthDto } from './dto'
+import * as argon from 'argon2'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime'
+import { error } from 'console'
 
 @Injectable()
 export class AuthService {
 	constructor(private prisma: PrismaService) {}
 
-	signin() {
-		return { msg: 'I have signed in' }
+	async signin(dto: AuthDto) {
+		// find user by email
+		const user = await this.prisma.user.findUnique({
+			where: {
+				email: dto.email
+			}
+		})
+
+		// if user does not exist throw exception
+		if (!user)
+			throw new ForbiddenException('Credentials incorrect')
+
+		// compare passwords
+		const pwMatches = await argon.verify(
+			user.hash,
+			dto.password
+		)
+
+		// if password incorrect throw exception
+		if (!pwMatches)
+			throw new ForbiddenException('Credentials incorrect')
+
+		// send back the user
+		delete user.hash
+		return user
 	}
 
-	signup() {
-		return { msg: 'I have signed up' }
+	async signup(dto: AuthDto) {
+		try {
+			// generate pwd hash
+			const hash = await argon.hash(dto.password)
+
+			// save new user into the DB
+			const user = await this.prisma.user.create({
+				data: {
+					email: dto.email,
+					hash
+				}
+			})
+
+			delete user.hash
+
+			// return the saved user
+			return user
+		} catch (error) {
+			if (error instanceof PrismaClientKnownRequestError) {
+				if (error.code === 'P2002') {
+					throw new ForbiddenException('Credentials taken')
+				}
+			}
+		}
+		throw error
 	}
 }
